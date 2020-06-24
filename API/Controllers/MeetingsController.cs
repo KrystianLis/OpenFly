cf41dcedf70c776a20aeb39f8fb1b1dc25cea142
Krystian Lis <krystian.lis@intel.com>
2020-06-24 20:18:37 +0200

    remove unncessary usings

--- API/Controllers/MeetingsController.cs
+++ API/Controllers/MeetingsController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
