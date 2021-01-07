86d6546e20a61b2d764d6db7d5a3b1517aed0e83
Krystian <lis.krystian.10@gmail.com>
2021-01-07 23:30:01 +0100

    refactor UserConfiguration

--- src/Infrastructure/Data/Config/UserConfiguration.cs
+++ src/Infrastructure/Data/Config/UserConfiguration.cs
@@ -8,11 +8,21 @@ namespace Infrastructure.Data.Config
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(25);
-            builder.Property(x => x.LastName).IsRequired().HasMaxLength(25);
-            builder.Property(x => x.Email).IsRequired().HasMaxLength(50);
-            builder.Property(x => x.UserName).IsRequired().HasMaxLength(25);
-            builder.Property(x => x.Password).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(25);
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(25);
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(x => x.UserName)
+                .IsRequired()
+                .HasMaxLength(25);
+            builder.Property(x => x.Password)
+                .IsRequired()
+                .HasMaxLength(50);
         }
     }
 }
