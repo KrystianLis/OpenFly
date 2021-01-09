6a00059e41d56a4e2cf438bb76fe06a9895c222f
Krystian <lis.krystian.10@gmail.com>
2021-01-09 16:08:56 +0100

    add account controller with login and register actions

---
+++ src/API/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using API.DTO;
+using API.Errors;
+using Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user is null)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+
+            return new UserDTO
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                UserName = user.UserName
+            };
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
+        {
+            var user = new User
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new ApiResponse(400));
+            }
+
+            return new UserDTO
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                UserName = user.UserName
+            };
+        }
+    }
+}
