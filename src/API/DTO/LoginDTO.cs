66838367691c50048de7c4bd028ffc6265576a22
Krystian <lis.krystian.10@gmail.com>
2021-01-16 17:52:43 +0100

    add Required attr to LoginDTO

--- src/API/DTO/LoginDTO.cs
+++ src/API/DTO/LoginDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
 {
     public class LoginDTO
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
