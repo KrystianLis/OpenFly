01cb2b51d992277a532f1f6a9e95dba7ceee8537
Krystian <lis.krystian.10@gmail.com>
2021-01-16 15:41:49 +0100

    add required attr for all addressDTO fields

--- src/API/DTO/AddressDTO.cs
+++ src/API/DTO/AddressDTO.cs
@@ -1,11 +1,20 @@
-﻿namespace API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
 {
     public class AddressDTO
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string ZipCode { get; set; }
+        [Required]
+        public string Country { get; set; }
     }
 }
