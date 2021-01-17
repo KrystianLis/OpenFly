33b8532f757b56e09463e16777f9c13b31a01411
Krystian <lis.krystian.10@gmail.com>
2021-01-17 20:52:17 +0100

    add ReferenceLoopHandling.Ignore

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
 
 namespace API
 {
@@ -22,7 +21,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
             services.AddDbContext<AppDbContext>(x =>
                 x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
 
