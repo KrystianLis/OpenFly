4dc213be5600109c7a1555858c50ffdc3950ee34
Krystian <lis.krystian.10@gmail.com>
2021-01-17 22:23:55 +0100

    add UserMeetingSpecification

---
+++ src/Core/Specification/UserMeetingSpecification.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+namespace Core.Specification
+{
+    public class UserMeetingSpecification : BaseSpecification<UserMeeting>
+    {
+        public UserMeetingSpecification(string userId, int meetingId)
+            : base(x => x.MeetingId == meetingId && Equals(x.UserId, userId))
+        {
+            AddInclude(x => x.Meeting);
+            AddInclude(x => x.User);
+        }
+    }
+}
